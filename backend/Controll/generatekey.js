ca83331602efb3cba2b40e3155ae8a954150db70
abdirhman615 <abdirhman615@gmail.com>
2023-08-11 11:30:57 -0800

    last

--- backend/Controll/generatekey.js
+++ backend/Controll/generatekey.js
@@ -1,3 +1,3 @@
-// const crypto = require('crypto')
-// const sectkey = crypto.randomBytes(32).toString('hex')
-// console.log("secrket key" ,sectkey)
+const crypto = require('crypto')
+const sectkey = crypto.randomBytes(32).toString('hex')
+console.log("secrket key" ,sectkey)
